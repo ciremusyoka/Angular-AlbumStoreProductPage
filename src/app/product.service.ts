8dd5575eeddc5e62076d55f735ac97a39dbf470d
ciremusyoka <wambua.eric01@gmail.com>
2019-10-26 15:00:01 +0300

    imported map

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http'
-// import rxjs/add/operator/ap
-import { map } from 'rxjs/operators';
+import { Http, Response } from '@angular/http'
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
@@ -11,9 +10,7 @@ export class ProductService {
 
   getAlbum(id:number) {
     return this._http.get(this._albumUrl)
-    .pipe(
-      map(response => response.json())
-    )
+      .map(response => response.json())
   }
 
 }
